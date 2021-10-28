a3dcd82014b502d1aad4aa1a2a327d7994882b3b
yddde <93260463+yddde@users.noreply.github.com>
2021-10-28 19:54:31 +0200

    Add files via upload

---
+++ userbot/__init__.py
@@ -0,0 +1,84 @@
+import time
+
+import heroku3
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import jmthon
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+
+__version__ = "4.0.0"
+__license__ = "كـتابة وتـعديل فريـق خليج ثون ."
+__author__ = "خليج ثون <https://T.ME/ZVVVN>"
+__copyright__ = "KhTHON TEAM (C) 2020 - 2021  " + __author__
+
+jmthon.version = __version__
+jmthon.tgbot.version = __version__
+LOGS = logging.getLogger("خليج ثون")
+bot = jmthon
+
+StartTime = time.time()
+JMVERSION = "4.0.0"
+
+ROZDEV = [
+    "1917795624",
+    "1917795624",
+    "1917795624",
+]
+
+if Config.UPSTREAM_REPO == "Telethon-Ksa":
+    UPSTREAM_REPO_URL = "https://github.com/yddde/SY-THON"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
+    else:
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# Variables
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
