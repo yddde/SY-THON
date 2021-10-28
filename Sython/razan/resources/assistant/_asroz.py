623f877d46955699ffe81912c22ebccd21e2b2db
yddde <93260463+yddde@users.noreply.github.com>
2021-10-28 20:02:33 +0200

    Add files via upload

---
+++ Sython/razan/resources/assistant/_asroz.py
@@ -0,0 +1,11 @@
+from userbot.Config import Config
+
+RBOT = Config.TG_BOT_USERNAME
+ROZA = Config.PRIVATE_GROUP_BOT_API_ID
+StJms = [
+    "▾∮ جـار اعـداد البـوت المسـاعد",
+    "▾∮ يـتم اكمـال اعـداد البـوت االمسـاعد",
+    "▾∮ تـم بنجاح اعـداد البـوت المسـاعد",
+]
+Stdec = "- 𝖪𝗁𝖳𝗁𝗈𝗇 𝖴𝗌𝖾𝗋𝖡𝗈𝗍 𝖠𝗌𝗌𝗂𝗌𝗍𝖺𝗇𝗍  \n- 𝖪𝗁𝖳𝗁𝗈𝗇 𝖥𝖮𝖱  {}\n- @zVVVn "
+SetAbt = "- 𝖪𝗁𝖳𝗁𝗈𝗇 𝖴𝖲𝖤𝖱𝖡𝖮𝖳\n- 𝖪𝗁𝖳𝗁𝗈𝗇 𝖥𝖮𝖱  {}"
